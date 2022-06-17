093ab1aaa685ec0124c558b5623f565948e94560
jakjr <jakjr@celepar.pr.gov.br>
2022-06-17 15:33:46 -0300

    Layout;

---
+++ resources/js/Shared/Box.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="box box-default mb-2">
+
+    <div class="box-header with-border d-flex" v-if="$slots.header">
+      <h4>
+        <slot name="header"></slot>
+      </h4>
+    </div>
+
+    <div class="box-body">
+      <slot/>
+    </div>
+
+    <div class="box-footer" v-if="$slots.footer">
+      <slot name="footer"></slot>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
